3f6a50004bdb367ee8868d1af3f3c3be29172713
Amr <amr.e.dev@gmail.com>
2015-06-25 13:54:46 +0200

    added pink

--- 2a/art.py
+++ 2a/art.py
@@ -2,12 +2,8 @@
 import turtle
 
 def draw_sq():
-	canv = turtle.Screen()
-	canv.bgcolor("white")
-
 	# get a turtule
 	chuck = turtle.Turtle()
-
 	# get it some personality
 	chuck.shape("turtle")
 	chuck.color("brown")
@@ -17,12 +13,21 @@ def draw_sq():
 		chuck.right(90)
 		chuck.speed(1)
 
-	# close canvas on mouse click
-	canv.exitonclick()
+def draw_circle():
+	pink = turtle.Turtle()
+	pink.shape("arrow")
+	pink.color("red")
+
+	pink.circle(20)
 
 
 def draw_art():
+	canv = turtle.Screen()
+	canv.bgcolor("white")
 	draw_sq()
+	draw_circle()
+	# close canvas on mouse click
+	canv.exitonclick()
 
 
 
