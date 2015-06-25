6cf1079bd09eb257b30517bee8fedbec4249c5b5
Amr <amr.e.dev@gmail.com>
2015-06-25 14:00:21 +0200

    finished art

--- 2a/art.py
+++ 2a/art.py
@@ -20,12 +20,23 @@ def draw_circle():
 
 	pink.circle(20)
 
+def draw_tri():
+	ringo = turtle.Turtle()
+	ringo.shape("turtle")
+	ringo.color("black")
+	ringo.speed(5)
+
+	for i in range(0, 3):
+		ringo.forward(70)
+		ringo.right(120)
+		ringo.forward(70)
 
 def draw_art():
 	canv = turtle.Screen()
 	canv.bgcolor("white")
 	draw_sq()
 	draw_circle()
+	draw_tri()
 	# close canvas on mouse click
 	canv.exitonclick()
 
