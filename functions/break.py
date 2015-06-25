2e69ae48e6bb68e74327b8e1ba71ccd75939d553
Amr <amr.e.dev@gmail.com>
2015-06-25 11:59:02 +0200

    loop

--- functions/break.py
+++ functions/break.py
@@ -1,8 +1,10 @@
 import webbrowser
 import time
 
-# delay
-time.sleep(10)
 
-# open Janis Joplin song
-webbrowser.open("https://www.youtube.com/watch?v=rX8hOw31wCQ")
+# loop three times a day
+for i in 3:
+	# delay
+	time.sleep(10)
+	# open Janis Joplin song
+	webbrowser.open("https://www.youtube.com/watch?v=rX8hOw31wCQ")
