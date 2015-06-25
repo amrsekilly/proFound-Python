170151f7839832f5ae2838f5b5b3a62defade1c6
Amr <amr.e.dev@gmail.com>
2015-06-25 12:23:13 +0200

    tst

---
+++ functions/secret/secret.py
@@ -0,0 +1,9 @@
+# reveals a secret message
+
+import os
+
+def rename_file():
+	filelst = os.listdir("/Users/amr/Documents/pyFoundation/functions/secret")
+
+	# make sure it's working
+	print filelst
