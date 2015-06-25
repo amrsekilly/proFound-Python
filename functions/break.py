77c11f9edc2c9d84bcf9bad87a6d00fb5f49882f
Amr <amr.e.dev@gmail.com>
2015-06-25 12:07:54 +0200

    prt statements

--- functions/break.py
+++ functions/break.py
@@ -3,10 +3,13 @@
 import webbrowser
 import time
 
+# pr started
+print "Break Program just started", time.ctime()
 
 # loop three times a day
 for i in range(0, 3):
 	# delay
 	time.sleep(10)
+	print "you have to take a break it's", time.ctime(), "now!"
 	# open Janis Joplin song
 	webbrowser.open("https://www.youtube.com/watch?v=rX8hOw31wCQ")
